9879395aa37cbec231102dcb0c1e8f8c975d1e48
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-11-10 21:06:50 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+// Markup Gallery
+const galleryListEl = document.querySelector('.gallery');
+
+const createMarkupElemetsGallery = ({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+const makeGalleryItem = galleryItems.map(createMarkupElemetsGallery).join('');
+
+galleryListEl.insertAdjacentHTML('beforeend', makeGalleryItem);
+
+// Markup Modal Elemets Galler
+const onGalleryImageClick = e => {
+  e.preventDefault();
+
+  if (e.code === 'Escape') {
+    modal.close();
+  }
+
+  const createMarkupModalElemetsGallery = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="auto" height="auto">`);
+
+  createMarkupModalElemetsGallery.show();
+
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      createMarkupModalElemetsGallery.close();
+    }
+  });
+};
+
+galleryListEl.addEventListener('click', onGalleryImageClick);
